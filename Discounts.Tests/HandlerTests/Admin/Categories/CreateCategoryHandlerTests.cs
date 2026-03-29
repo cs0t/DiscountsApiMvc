4195a6c4fb4a31d58387e1eb57723d600037cfaf
cs0t <sabaodisharia04@gmail.com>
2026-03-30 01:42:52 +0400

    Start refactoring unit tests for handlers!

---
+++ Discounts.Tests/HandlerTests/Admin/Categories/CreateCategoryHandlerTests.cs
@@ -0,0 +1,24 @@
+using System.Linq.Expressions;
+using Discounts.Application.Handlers.Admin.Categories;
+using Discounts.Application.Interfaces.RepositoryContracts;
+using Discounts.Domain.Entities;
+using Moq;
+
+namespace Discounts.Tests.HandlerTests.Admin.Categories;
+
+public class CreateCategoryHandlerTests
+{
+    private readonly Mock<ICategoryRepository> _categoryRepositoryMock = new();
+
+    [Fact]
+    public async Task CreateCategoryHandler_ShouldThrowException_WhenCategoryWithSameNameExists()
+    {
+        _categoryRepositoryMock
+            .Setup(repo => 
+                repo.ExistsAsync(It.IsAny<Expression<Func<Category, bool>>>(),  It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        var handler = new CreateCategoryHandler(_categoryRepositoryMock.Object);
+        
+    }
+}
---
+++ Discounts.Tests/HandlerTests/Admin/Categories/UpdateCategoryHandlerTests.cs
@@ -0,0 +1,6 @@
+namespace Discounts.Tests.HandlerTests.Admin.Categories;
+
+public class UpdateCategoryHandlerTests
+{
+    
+}
